d79300609a0173ba6e869545c8d34268584e31da
hlajungo <hlajungo@gmail.com>
2024-12-28 01:42:13 +0800

    Update logexec.sh

--- logexec/logexec.sh
+++ logexec/logexec.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-scriptName="$(basename "$(realpath "${BASH_SOURCE[0]}")")"
-scriptDir="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
-cd $scriptDir
+parentScriptName="$(basename "$(realpath "${BASH_SOURCE[1]}")")"
+parentScriptDir="$(dirname "$(realpath "${BASH_SOURCE[1]}")")"
 
-source color.sh
+source "color.sh"
 
 logexec()
 {
@@ -12,19 +11,19 @@ logexec()
   local backNLine=0
   
   # track to first not empty line
-  while [[ -z $(awk "NR==${BASH_LINENO[0]}-$backNLine {print prev} {prev=\$0}" "$scriptDir/$scriptName") ]]
+  while [[ -z $(awk "NR==$1-$backNLine {print prev} {prev=\$0}" "$parentScriptDir/$parentScriptName") ]]
   do
     backNLine=$(($backNLine+1))
   done
 
   # 如果上上行是 "\" 結尾的，推入 cmdStr
-  while [[ "$(awk "NR==${BASH_LINENO[0]}-$backNLine-1 {print prev} {prev=\$0}" "$scriptDir/$scriptName")" == *"\\" ]]; do
-    cmdStr+=("$(awk "NR==${BASH_LINENO[0]}-$backNLine {print prev} {prev=\$0}" "$scriptDir/$scriptName")")
+  while [[ "$(awk "NR==$1-$backNLine-1 {print prev} {prev=\$0}" "$parentScriptDir/$parentScriptName")" == *"\\" ]]; do
+    cmdStr+=("$(awk "NR==$1-$backNLine {print prev} {prev=\$0}" "$parentScriptDir/$parentScriptName")")
     backNLine=$(($backNLine+1))
   done
   
   # 輸出最後一行
-  color yellow "$(awk "NR==${BASH_LINENO[0]}-$backNLine {print prev} {prev=\$0}" "$scriptDir/$scriptName")"
+  color yellow "$(awk "NR==$1-$backNLine {print prev} {prev=\$0}" "$parentScriptDir/$parentScriptName")"
 
   # 吐出 cmdStr
   while [[ ${#cmdStr[@]} -ne 0 ]]; do
@@ -32,15 +31,10 @@ logexec()
     unset cmdStr[-1]
   done
 
-  if [ $1 -eq 0 ]; then
-    color green "$2"
+  if [ $2 -eq 0 ]; then
+    color green "$3"
   else
-    color red "$3"
+    color red "$4"
   fi
 }
 
-cat t.txt
-
-logexec $? \
-  "SUC" \
-  "ERR"
